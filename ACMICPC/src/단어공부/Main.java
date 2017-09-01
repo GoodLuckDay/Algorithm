fcfed9d00e06002ca263d7fb96f0eb7c71ef733a
Goodluckdays <haon4658@gmail.com>
2017-09-01 22:36:48 +0900

    필요없는 파일 삭제

--- ACMICPC/src/단어공부/Main.java
+++ ACMICPC/src/단어공부/Main.java
@@ -1,43 +1,35 @@
 package 단어공부;
 
-
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        HashMap<Character,Integer> hashMap = new HashMap<>();
-        String str = scanner.nextLine();
-        str = str.toLowerCase();
-        char[] chars = str.toCharArray();
-        for(int i=0; i<str.length(); i++){
-            if(hashMap.containsKey(chars[i])){
-                int temp = hashMap.get(chars[i]);
-                hashMap.replace(chars[i],temp+1);
-            }
-            else{
-                hashMap.put(chars[i],1);
-            }
+        String inputStr = scanner.nextLine();
+        char[] array = inputStr.toLowerCase().toCharArray();
+        int[] solve = new int[27];
+        for(int i=0; i<array.length; i++){
+            solve[array[i]-96]+=1;
         }
-        int maxCount = -1;
-        char maxChar = ' ';
+        int max = Integer.MIN_VALUE;
+        int index = -1;
         boolean multi = false;
-        for(Character key : hashMap.keySet()){
-            if(maxCount < hashMap.get(key)){
-                maxCount = hashMap.get(key);
-                maxChar = key;
-                multi = false;
-            }
-            else if(maxCount == hashMap.get(key)){
+        for(int i=0; i<solve.length; i++){
+            if(max == solve[i]){
                 multi = true;
+            }
+            else if(max < solve[i]){
+                index = i;
+                max = solve[i];
+                multi = false;
             }
         }
         if(multi){
-            System.out.println('?');
+            System.out.println("?");
         }
         else{
-            System.out.println(Character.toUpperCase(maxChar));
+            char temp = (char) (index + 64);
+            System.out.println(temp);
         }
     }
 }
-
